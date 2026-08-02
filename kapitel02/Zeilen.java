cfddf10bcb33cbe7b7bb61912505ef772564ec3a
Jdon5 <162381746+Jdon5@users.noreply.github.com>
2026-08-02 19:29:16 +0200

    Kapitel 2: Aufgabe 3 - Mehrzeilige Ausgabe mit print

---
+++ kapitel02/Zeilen.java
@@ -0,0 +1,8 @@
+package kapitel02;
+
+public class Zeilen{
+    public static void main(String[] args){
+        /*Mehrzeilige Ausgabe von Zeichenkette*/ 
+        System.out.print("Hallo,\nmein Name ist: \n Jesse Osei"); 
+    }
+}
