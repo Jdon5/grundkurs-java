d5b4428f5a9a9b88911f071ad38bc2f8efb633ff
Jdon5 <162381746+Jdon5@users.noreply.github.com>
2026-08-02 19:45:01 +0200

    Kapitel 2: Aufgabe 4 - Hex- und Binärzahl in int-Variablen

---
+++ kapitel02/Zahlen.java
@@ -0,0 +1,15 @@
+package kapitel02;
+
+/**
+ * Zahlen
+ */
+public class Zahlen {
+    public static void main(String[] args) {
+        int hexadezimalZahl = 0xAA00; // Hexadezimal-Literal (Präfix 0x)
+        int binaerZahl = 0b10101010; // Binär-Literal (Präfix 0b)
+
+        //Ausgabe
+        System.out.println("Hexadezimalzahl: "+ hexadezimalZahl);
+        System.out.println("Binaere Zahl: "+ binaerZahl);
+    }
+}
